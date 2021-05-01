7670e6b580fd48e6111c0df724ef0c7fbb1d4e8a
erkanekici <erkan>
2021-05-01 18:36:11 +0300

    Half Pyramid Pattern

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String []args){
+        //Verilen satir sayisi kadar * karakteriyle yarim piramit olusturma
+        int rows = 5;
+        int sequence = 1;
+        pattern(rows, sequence);
+    }
+
+    static void pattern(int rows, int sequence)
+    {
+        if (rows == 0)
+            return;
+
+        printn(sequence);
+        //printn(rows); //reverse
+
+        System.out.println();
+
+        // recursively calling pattern()
+        pattern(rows - 1, sequence + 1);
+    }
+
+    static void printn(int num){
+        if (num == 0)
+            return;
+
+        System.out.print ("* ");
+
+        //recursively calling printn()
+        printn(num - 1);
+    }
+}
